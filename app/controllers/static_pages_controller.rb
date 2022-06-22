20d32cb785a9ecbdf0de5cc6c52cb48fee74a004
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-06-22 00:58:22 -0500

    Applies Heroku configuration

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Static pages controller
+class StaticPagesController < ApplicationController
+  def home; end
+end
---
+++ app/helpers/static_pages_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Static pages helper
+module StaticPagesHelper
+end
