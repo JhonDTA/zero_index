2fb8209220620ab74ca8a3a71d9ecc5f445ea5b0
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-06-23 23:07:10 -0500

    Changes :host key for :domain in email configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'zeroindex.herokuapp.com' }
   config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { host: 'zeroindex.herokuapp.com' }
+  config.action_mailer.mailgun_settings = { domain: 'zeroindex.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
