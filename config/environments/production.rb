1afc05af802de6858c4d8008ccb17c35bbf67f71
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-06-23 23:05:22 -0500

    Uses default url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,8 +70,9 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.default_url_options = { host: 'zeroindex.herokuapp.com' }
   config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { host: ENV.fetch('HOST', 'zeroindex.herokuapp.com') }
+  config.action_mailer.mailgun_settings = { host: 'zeroindex.herokuapp.com' }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
