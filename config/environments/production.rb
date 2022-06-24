df8bfa3e58bd4916a6224f9e200aa5290c40058a
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-06-23 22:58:48 -0500

    Uses a fallback host name

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,7 +71,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { host: ENV.fetch('HOST', '') }
+  config.action_mailer.mailgun_settings = { host: ENV.fetch('HOST', 'zeroindex.herokuapp.com') }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
