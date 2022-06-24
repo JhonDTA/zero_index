560f509686e4fee6bd09cac0999a82288c1465ad
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-06-23 23:17:23 -0500

    Uses standard MailGun configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,15 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: 'zeroindex.herokuapp.com' }
-  config.action_mailer.delivery_method = :mailgun
-  config.action_mailer.mailgun_settings = { domain: 'zeroindex.herokuapp.com' }
+  ActionMailer::Base.smtp_settings = {
+    :port => ENV['MAILGUN_SMTP_PORT'],
+    :address => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name => ENV['MAILGUN_SMTP_LOGIN'],
+    :password => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain => 'zeroindex.herokuapp.com',
+    :authentication => :plain,
+  }
+  ActionMailer::Base.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
