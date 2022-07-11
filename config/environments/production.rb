0a31eb5f762d59f26a1cc82b5ad88515376f5957
Jhonatan Daniel Tovar Arreguin <jhondta@icloud.com>
2022-07-11 00:51:25 -0500

    Solves MailGun mail sending exception

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.default_url_options = { host: ENV.fetch('APP_DOMAIN', nil) }
+  configure_mailer
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
